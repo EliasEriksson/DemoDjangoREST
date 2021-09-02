d025f9b9ee3b408b261b32608a4b07009b505efd
EliasEriksson <EliasEriksson95@gmail.com>
2021-09-02 10:23:22 +0200

    update

--- djangoRESTRoot/REST/urls.py
+++ djangoRESTRoot/REST/urls.py
@@ -1,12 +1,17 @@
-from django.urls import path
+from django.urls import path, re_path
 from django.urls import include
 from . import views
 from rest_framework import routers
+from django.http import HttpResponseRedirect
+from rest_framework.authtoken.views import obtain_auth_token
+
 
 router = routers.DefaultRouter()
 router.register("courses", views.CourseViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
-    path("api-auth/", include("rest_framework.urls"), name="rest_framework")
+    path("token/", obtain_auth_token),
+    re_path(r"^auth/$", lambda request: HttpResponseRedirect("login/"), name="api-redirect"),
+    path("auth/", include("rest_framework.urls"), name="rest_framework")
 ]
--- djangoRESTRoot/REST/views.py
+++ djangoRESTRoot/REST/views.py
@@ -1,12 +1,14 @@
-from django.http import HttpRequest, HttpResponse
-from .serializers import Course, CoursesSerializer
 from rest_framework import viewsets
 from .models import Course
 from .serializers import CoursesSerializer
+from rest_framework import permissions
+from rest_framework.authentication import SessionAuthentication, TokenAuthentication
 # Create your views here.
 
 
 class CourseViewSet(viewsets.ModelViewSet):
+    authentication_classes = [SessionAuthentication, TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
     queryset = Course.objects.all()
     serializer_class = CoursesSerializer
 
